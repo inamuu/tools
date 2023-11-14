a31de30e2764874297a7e9a780fa68c406313ff1
inamuu <8310973+inamuu@users.noreply.github.com>
2023-11-14 15:43:56 +0900

    get records from a given table

--- mysql/check_records/python/main.py
+++ mysql/check_records/python/main.py
@@ -12,15 +12,15 @@ load_dotenv()
 db_host = os.getenv('DB_HOST')
 db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
+db_password = os.getenv('DB_PASS')
 
 # Function to get the first record of a given table
 
 
-def get_first_record(table, cursor):
-    query = f"SELECT * FROM {table} ORDER BY 1 DESC LIMIT 1"
+def get_records(table, cursor):
+    query = f"SELECT * FROM {table} ORDER BY 1 DESC LIMIT 3"
     cursor.execute(query)
-    return cursor.fetchone()
+    return cursor.fetchall()
 
 # Main execution block
 
@@ -38,7 +38,7 @@ def main():
         print(f"Error connecting to MySQL: {e}")
         return
 
-    cursor = connection.cursor()
+    cursor = connection.cursor(buffered=True)
 
     # Read table names from list.txt
     try:
@@ -55,9 +55,10 @@ def main():
         # Fetch the first record from each table and write to the CSV file
         for table in tables:
             try:
-                record = get_first_record(table, cursor)
-                csv_writer.writerow([table] + list(record))
-                print(f"First record from {table} written to CSV.")
+                records = get_records(table, cursor)
+                for record in records:
+                    csv_writer.writerow([table, 'Record'] + list(record))
+                print(f"Last records from {table} written to CSV.")
             except mysql.connector.Error as e:
                 print(f"Error querying table {table}: {e}")
 
