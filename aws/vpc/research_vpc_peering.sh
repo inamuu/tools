39263a9eb677aa23c6db7e86d93779938995be43
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2025-07-07 14:17:11 +0900

    display AWS account number with profile name

--- aws/vpc/research_vpc_peering.sh
+++ aws/vpc/research_vpc_peering.sh
@@ -3,7 +3,8 @@
 AWS_PROFILE_LIST=$(cat ~/.aws/config | egrep "profile" | sed -E 's/(\[|\]|profile )//g')
 
 for i in $(echo ${AWS_PROFILE_LIST});do
-  printf "\nAWS Account: $i\n"
+  printf "\nAWS Account: $i, "
+  aws sts get-caller-identity --no-cli-pager --query "Account" --output text --profile $i
   aws ec2 describe-vpc-peering-connections --query "VpcPeeringConnections[*].{PeeringConnectionId:VpcPeeringConnectionId,Requester:RequesterVpcInfo.VpcId,Accepter:AccepterVpcInfo.VpcId,Status:Status.Code}" --output table --no-cli-pager --profile $i
   aws ec2 describe-vpcs --query "Vpcs[*].{VpcId:VpcId,Name:Tags[?Key=='Name'].Value|[0]}" --output table --no-cli-pager --profile $i
 done
